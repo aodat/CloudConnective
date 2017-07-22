aa5d8d5bdfa4b31e3cb7b16e9497ea0780642646
Abdalla Odat <macbook@Macbooks-MacBook-Pro.local>
2017-07-22 23:32:09 +0300

    Initial Commit

---
+++ CloudConnective/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CloudConnective
+//
+//  Created by Abdalla Odat on 7/22/17.
+//  Copyright © 2017 Abdalla Odat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
